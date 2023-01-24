40dd2a900d83ecd01405ff7046ddd28da24f3fdc
AndriiSobchuk <86925275+AndriiSobchuk@users.noreply.github.com>
2023-01-25 00:23:07 +0200

    Update k8s_master_install_kubeadm.sh

--- master_node/k8s_master_install_kubeadm.sh
+++ master_node/k8s_master_install_kubeadm.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -x
 echo "Start master node requirements installation..."
 
 # Install Docker
+
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --batch --yes
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # 2. Install cri-dockerd
 git clone https://github.com/Mirantis/cri-dockerd.git
