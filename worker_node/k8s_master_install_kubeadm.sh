3415209e470362cb3bf6658a23614a2e778a0224
Andrii Sobchuk <anri.sobchuk@gmail.com>
2023-01-25 15:43:58 +0000

    Add installation script for worker node

---
+++ worker_node/k8s_master_install_kubeadm.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -x 
+
+echo "Start..."
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+###########
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+git clone https://github.com/Mirantis/cri-dockerd.git
+
+# 2.1 Install GO
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+source ~/.bash_profile
+
+
+# 2.2 Enable cri-docker service (probably with sudo)
+cd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+sudo install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+sudo cp -a packaging/systemd/* /etc/systemd/system
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+echo "Joining k8s cluster..."
+
+##### Link worker to master
+sudo kubeadm join $K8S_MASTER:$K8S_PORT --token wxjhr8.ufgalefxewn807yh --discovery-token-ca-cert-hash sha256:4800107f867b1165a522c70815d817118a20f15fbaedc4d858bb483b9a671e43 --cri-socket=unix:///var/run/cri-dockerd.sock
+
+echo "Installation finished! Worker is ready!"
